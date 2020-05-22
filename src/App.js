268acb651e8e857b7dbcfba8891cb3bf8ad1ee6a
jherran <jorge.herran@endava.com>
2020-05-21 20:31:45 -0500

    first change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Excelente REACT APPS
+          Excelente REACT APPS NEW DEPLOY
         </p>
         <a
           className="App-link"
